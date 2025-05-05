17ad6c9989f0a783cb32bb8c1c3fd3de4c300c92
Mohd Faziel <mohdfazel969@gmail.com>
2025-05-05 23:59:58 +0530

    Added new animation to home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -12,6 +12,7 @@ import { HiOutlineMail } from "react-icons/hi";
 import resume from "../../assets/MohdFaziel_Resume.pdf";
 import ResumeButton from "./ResumeButton.jsx";
 import SEO from "../SEO/SEO";
+import RotatingText from "../../utils/RotatingText.jsx";
 
 export default function Home() {
   const [text] = useTypewriter({
@@ -21,11 +22,11 @@ export default function Home() {
 
   return (
     <>
-      <SEO 
+      <SEO
         title="Mohd Faziel - Web Developer Portfolio | Home"
         description="MERN Stack Developer specializing in creating cutting-edge, user-centric web solutions. Explore my projects and skills in web development."
       />
-      <section 
+      <section
         className="MainHome w-full px-[2rem] md:px-[10rem] text-active py-[1rem] md:py-[5rem] bg-dark1 min-h-screen flex justify-center items-center"
         aria-label="Home Section"
       >
@@ -56,7 +57,12 @@ export default function Home() {
                 viewport={fadeIn("down", 0.3).viewportOptions}
                 className="absolute -top-9 md:-top-16 -left-10"
               >
-                <img width="40px" className="md:w-[70px]" src={Logo} alt="Personal Logo" />
+                <img
+                  width="40px"
+                  className="md:w-[70px]"
+                  src={Logo}
+                  alt="Personal Logo"
+                />
                 <h1 className="name text-5xl md:text-6xl">
                   Mohd <br />{" "}
                   <span className="border-b-4 rounded-sm border-main">Faz</span>
@@ -73,35 +79,74 @@ export default function Home() {
             className="Right w-full md:w-[50%] h-full flex justify-start items-center gap-3 md:gap-12"
           >
             <div className="Info flex flex-col gap-2 justify-center items-start">
-              <h2 className="text-6xl md:text-7xl">I'm</h2>
-              <p className="text-3xl md:text-6xl text-main" role="text" aria-label="Professional roles">
-                {text}
-                <span className="text-main">
-                  <Cursor cursorStyle="|" />
-                </span>
-              </p>
+              <h className="text-6xl md:text-7xl">I'm</h>
+              <RotatingText
+                texts={["Web Developer", "Coder", "Programmer"]}
+                mainClassName="text-main overflow-hidden text-3xl md:text-6xl"
+                staggerFrom={"last"}
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                exit={{ y: "-120%" }}
+                staggerDuration={0.025}
+                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                rotationInterval={2000}
+              />
               <p className="text-sm md:text-lg text-justify w-[18rem] md:w-[30rem] text-unactive">
-                MERN Developer. Dedicated to crafting
-                cutting-edge, user-centric web solutions with a creative touch.
+                MERN Developer. Dedicated to crafting cutting-edge, user-centric
+                web solutions with a creative touch.
               </p>
-              <a href={resume} target="_blank" rel="noopener noreferrer" aria-label="Download Resume">
+              <a
+                href={resume}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Download Resume"
+              >
                 <ResumeButton />
               </a>
             </div>
-            <nav className="SocialMedia flex flex-col justify-center items-center gap-5 md:gap-7" aria-label="Social Media Links">
-              <a href="https://www.linkedin.com/in/mohdfaziel/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Profile">
+            <nav
+              className="SocialMedia flex flex-col justify-center items-center gap-5 md:gap-7"
+              aria-label="Social Media Links"
+            >
+              <a
+                href="https://www.linkedin.com/in/mohdfaziel/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="LinkedIn Profile"
+              >
                 <FaLinkedinIn className="transition-all md:scale-150 md:hover:text-main hover:scale-150" />
               </a>
-              <a href="mailto:mohdfazel969@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email Contact">
+              <a
+                href="mailto:mohdfazel969@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Email Contact"
+              >
                 <HiOutlineMail className="transition-all md:scale-150 md:hover:text-main hover:scale-150" />
               </a>
-              <a href="https://github.com/mohdfaziel" target="_blank" rel="noopener noreferrer" aria-label="GitHub Profile">
+              <a
+                href="https://github.com/mohdfaziel"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="GitHub Profile"
+              >
                 <ImGithub className="transition-all md:scale-150 md:hover:text-main hover:scale-150" />
               </a>
-              <a href="https://leetcode.com/u/mohdfaziel/" target="_blank" rel="noopener noreferrer" aria-label="LeetCode Profile">
+              <a
+                href="https://leetcode.com/u/mohdfaziel/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="LeetCode Profile"
+              >
                 <SiLeetcode className="transition-all md:scale-150 md:hover:text-main hover:scale-150" />
               </a>
-              <a href="https://www.instagram.com/heyy.faziel?igsh=MWQxdW1yNDJ5ZGVicA==" target="_blank" rel="noopener noreferrer" aria-label="Instagram Profile">
+              <a
+                href="https://www.instagram.com/heyy.faziel?igsh=MWQxdW1yNDJ5ZGVicA=="
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Instagram Profile"
+              >
                 <FaInstagram className="transition-all md:scale-150 md:hover:text-main hover:scale-150" />
               </a>
             </nav>
