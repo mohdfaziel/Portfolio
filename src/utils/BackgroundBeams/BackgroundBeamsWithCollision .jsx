9db11b942eccb88e2fb63062229a4b9498ee1eaa
Mohd Faziel <mohdfazel969@gmail.com>
2025-05-06 20:15:25 +0530

    Fix

--- src/utils/BackgroundBeams/BackgroundBeamsWithCollision .jsx
+++ src/utils/BackgroundBeams/BackgroundBeamsWithCollision .jsx
@@ -67,7 +67,7 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
-        "h-96 md:h-[40rem] bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800 relative flex items-center w-full justify-center overflow-hidden",
+        "h-screen md:h-[40rem] bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800 relative flex items-center w-full justify-center overflow-hidden",
         // h-screen if you want bigger
         className
       )}>
