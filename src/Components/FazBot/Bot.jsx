eb6b1ea835a8818c5fcc5973410eab99d63322cf
Mohd Faziel <mohdfazel969@gmail.com>
2025-10-09 14:15:09 +0530

    changed the model of gemeni

--- src/Components/FazBot/Bot.jsx
+++ src/Components/FazBot/Bot.jsx
@@ -21,7 +21,7 @@ function Bot() {
     }
 
     const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
     chatRef.current = model.startChat({
       history: [
